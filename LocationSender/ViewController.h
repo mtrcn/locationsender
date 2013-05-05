aba6d32ee9575b60c5200f1ad04ccb06fe97eac8
Mete Ercan Pakdil <mete@pakdil.org>
2013-05-05 16:45:51 +0300

    Initial Commit

---
+++ LocationSender/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LocationSender
+//
+//  Created by Mete Pakdil on 5/5/13.
+//  Copyright (c) 2013 Mete Ercan Pakdil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ LocationSender/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LocationSender
+//
+//  Created by Mete Pakdil on 5/5/13.
+//  Copyright (c) 2013 Mete Ercan Pakdil. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
