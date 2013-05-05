964b0a60b1efc2ba77baf2de7d7fa7fec6822d59
Mete Ercan Pakdil <mete@pakdil.org>
2013-05-05 21:22:22 +0300

    Initial Commit

--- LocationSender/ViewController.h
+++ LocationSender/ViewController.h
@@ -7,7 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+#import "AFNetworking.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
+@property (strong, nonatomic) IBOutlet MKMapView *map;
+@property (strong, nonatomic) IBOutlet UILabel *lblLocation;
+@property (strong, nonatomic) IBOutlet UITextField *txtMessage;
+@property (strong, nonatomic) IBOutlet UITextField *txtUsername;
+@property (strong, nonatomic) IBOutlet UIButton *btnSend;
+- (IBAction)sendLocation:(id)sender;
+- (IBAction)hideKeyboard:(id)sender;
 
 @end
--- LocationSender/ViewController.m
+++ LocationSender/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    CLLocationManager *locationManager;
+    CLLocation* location;
+}
 
 @end
 
@@ -17,13 +20,68 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    locationManager = [[CLLocationManager alloc] init];
+    locationManager.delegate = self;
+    [locationManager startUpdatingLocation];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    location = [locations lastObject];
+    _lblLocation.text = [NSString stringWithFormat:@"%.6f, %.6f",location.coordinate.latitude, location.coordinate.longitude];
+    
+    MKCoordinateRegion viewRegion = MKCoordinateRegionMake(location.coordinate, MKCoordinateSpanMake(1, 1));
+    [_map setRegion:viewRegion animated:YES];
+    [_map removeAnnotations:[_map annotations]];
+    MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+    [annotation setCoordinate:location.coordinate];
+    [annotation setTitle:@"You\'re Here"];
+    [_map addAnnotation:annotation];
+    
+}
+
+- (IBAction)sendLocation:(id)sender {
+    
+    NSString* username = [_txtUsername text];
+    NSString* message = [_txtMessage text];
+    if ([username length] > 0 && [message length] > 0 && location != nil){
+        _btnSend.enabled = FALSE;
+        NSURL *url = [NSURL URLWithString:@"http://localhost:8080"];
+        AFHTTPClient *httpClient = [[AFHTTPClient alloc] initWithBaseURL:url];
+        [httpClient setParameterEncoding:AFJSONParameterEncoding];
+        NSDictionary* params = [NSDictionary dictionaryWithObjectsAndKeys:username, @"username", message, @"message", [NSNumber numberWithDouble:location.coordinate.latitude], @"lat", [NSNumber numberWithDouble:location.coordinate.longitude], @"lng", nil];
+        NSMutableURLRequest *request = [httpClient requestWithMethod:@"POST"
+                                                                path:@"/locationcather/rest/catch"
+                                                          parameters:params];
+        [request setValue:[NSString stringWithFormat:@"application/json"] forHTTPHeaderField:@"Accept"];
+        AFHTTPRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+            UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Message Sent" message:@"Your message successfully sent." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:YES];
+            _btnSend.enabled = TRUE;
+        } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON){
+            NSLog(@"%@", [error userInfo]);
+            NSDictionary *dictionary = [response allHeaderFields];
+            NSLog(@"%@", [dictionary description]);
+            UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Error" message:[NSString stringWithFormat:@"An error happened. Code %d", response.statusCode] delegate:self cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [alert performSelectorOnMainThread:@selector(show) withObject:nil waitUntilDone:YES];
+            _btnSend.enabled = TRUE;
+        }];
+        [httpClient registerHTTPOperationClass:[AFJSONRequestOperation class]];
+        [operation start];
+        _btnSend.enabled = TRUE;
+
+    }else{ //username or message is empty
+        UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Validation Error" message:@"Location, Sent By or Message cannot be empty." delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    }
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];// this will do the trick
 }
 
 @end
